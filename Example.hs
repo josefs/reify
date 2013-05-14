1db82bea197d0910391d8c7eaae3c299c6c30282
Josef Svenningsson <josef.svenningsson@gmail.com>
2013-05-14 09:44:08 +0200

    Add our example language.

--- Example.hs
+++ Example.hs
@@ -1,2 +1,18 @@
 {-# LANGUAGE GADTs #-}
 module Example where
+
+import Data.IORef
+
+newtype R a = R { unR :: IORef a }
+
+data S a where
+  Ret  :: E a -> S a
+  Bind :: S a -> (E a -> S b) -> S b
+  New  :: E a -> S (R a)
+  Get  :: E (R a) -> S a
+  Set  :: E (R a) -> E a -> S ()
+
+data E a where
+  Int :: Int -> E Int
+  Add :: E a -> E a -> E a'
+
