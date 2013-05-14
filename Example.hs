703fecc35062be288896ea7cbd5e0ff53ae7a490
Josef Svenningsson <josef.svenningsson@gmail.com>
2013-05-14 09:53:48 +0200

    Solution 1 for our example

--- Example.hs
+++ Example.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE Rank2Types #-}
 module Example where
 
 import Data.IORef
@@ -16,3 +17,22 @@ data E a where
   Int :: Int -> E Int
   Add :: E a -> E a -> E a'
 
+-- Solution 1
+
+data M a = M { unM :: (forall b. (a -> S b) -> S b) }
+
+instance Monad M where
+  return a = M (\k -> k a)
+  M f >>= g = M (\k -> f (\a -> unM (g a) k))
+
+new :: E a -> M (E (R a))
+new a = M (\k -> Bind (New a) k)
+
+get :: E (R a) -> M (E a)
+get r = M (\k -> Bind (Get r) k)
+
+set :: E (R a) -> E a -> M (E ())
+set r a = M (\k -> Bind (Set r a) k)
+
+runM :: M (E a) -> S a
+runM (M f) = f Ret
